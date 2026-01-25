77c3fb4338d6cff4527f76327b20d682fa42ddad
Johnny <johnny@johnnys-imac.home>
2026-01-25 20:25:42 +0000

    Fix logo to link back to /home

--- components/nav/TopNav.tsx
+++ components/nav/TopNav.tsx
@@ -41,7 +41,7 @@ export default function TopNav() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <Link href={user ? "/search" : "/"} className="text-xl font-bold text-gray-900">
+          <Link href={user ? "/home" : "/"} className="text-xl font-bold text-gray-900">
             ClientDining
           </Link>
 
