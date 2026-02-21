1ac297014840d7159abfbf3b42bba72a8483af0b
Johnny Hall <john@clientdining.com>
2026-02-21 22:40:50 +0000

    tiering remove

--- components/modals/PremiumUnlockModal.tsx
+++ components/modals/PremiumUnlockModal.tsx
@@ -1,4 +1,4 @@
 // Tier system removed. This component is no longer in use.
-export default function PremiumUnlockModal() {
+export default function PremiumUnlockModal({ isOpen, onClose }: { isOpen?: boolean; onClose?: () => void }) {
   return null
 }
