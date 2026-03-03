74e73ef2af51b9ee1b47b9258a36ba4da104c42b
Johnny Hall <john@clientdining.com>
2026-03-03 15:46:05 +0000

    party size modal

--- app/(diner)/alerts/page.tsx
+++ app/(diner)/alerts/page.tsx
@@ -94,7 +94,7 @@ export default function AlertsPage() {
               area,
               address,
               postcode,
-              image_venue
+              image_venue,
               requires_guest_names
             )
           )
