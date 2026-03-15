37f563b2f1d37babef618c3612549213b7aaaf40
Johnny Hall <john@clientdining.com>
2026-03-15 19:37:34 +0000

    less space to footer

--- app/(diner)/layout.tsx
+++ app/(diner)/layout.tsx
@@ -18,7 +18,7 @@ export default function DinerLayout({
   return (
     <div className="bg-zinc-50 flex flex-col">
       <TopNav />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-24 w-full">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-8 w-full">
         {children}
       </main>
       <Footer />
