c63009a040abfba926bd444129dacb1b957ee805
Johnny Hall <john@clientdining.com>
2026-03-25 19:56:14 +0000

    wide open

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -31,7 +31,7 @@ export default function LandingPage() {
   }, [])
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-zinc-50">
 
       {/* Nav */}
       <header className="absolute top-0 left-0 right-0 z-50">
